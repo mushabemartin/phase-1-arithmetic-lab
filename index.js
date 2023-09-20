96433d9eb47c03509a9a442b19d9188fba95c144
MUSHABE <mushabe.martin@student.moringaschool.com>
2023-09-20 16:49:01 +0300

    javascript commit

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31;
+let num2 = 2;
+let multiply =  (num1*num2);
+let random = Math.floor(Math.random() * 10);
+console.log (random)
+let num3 = 20;
+let num4 = 8;
+let mod = (num3 % num4);
+console.log (mod)
+let max = Math.max(2, 3, 6, 20, 10)
+console.log(max)
+
 
 // Write your code here
